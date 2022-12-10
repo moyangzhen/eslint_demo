914796304deac9eef7ffe047ea99b0d8c36f3c2b
moyangzhen <15711476734@163.com>
2022-12-10 10:11:51 +0800

    测试冲突

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="查看是否与github有区别,重新出" />
+    <HelloWorld msg="查看是否与github有区别,不同分支修改同一文件" />
   </div>
 </template>
 
